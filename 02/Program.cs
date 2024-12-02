9c43ca14beb9a996e66a4350a6a4b78da5e55b3c
HAEMIC <Michael.Haemmerle@illwerkevkw.at>
2024-12-02 08:22:50 +0100

    added second day

---
+++ 02/Program.cs
@@ -0,0 +1,24 @@
+﻿using _02;
+
+using (StreamReader reader = new StreamReader("../../../input.txt"))
+{
+    List<List<int>> reports = new();
+
+    string? newLine = reader.ReadLine();
+
+    while (newLine is not null)
+    {
+        List<string> numbersString = newLine.Split(" ").ToList();
+        List<int> numbers = numbersString.Select(int.Parse).ToList();
+
+        reports.Add(numbers);
+
+        newLine = reader.ReadLine();
+    }
+
+    var result1 = reports.Count(x => XmasValidator.Verify(x, 0));
+    var result2 = reports.Count(x => XmasValidator.VerifyAlternate(x));
+
+    Console.WriteLine($"result for number 1: {result1}");
+    Console.WriteLine($"result for number 2: {result2}");
+}
---
+++ 02/XmasValidator.cs
@@ -0,0 +1,71 @@
+﻿namespace _02;
+
+public class XmasValidator
+{
+    public static bool Verify(List<int> list, int tolerance = 0)
+    {
+        return (Math.Min(CountAscendingErrors(list), CountDescendingErrors(list)) + CountDistanceErrors(list)) <= tolerance;
+    }
+
+    public static bool VerifyAlternate(List<int> list)
+    {
+        for (int i = 0; i < list.Count; i++)
+        {
+            var tempList = new List<int>(list);
+            tempList.RemoveAt(i);
+            if (Verify(tempList)) return true;
+        }
+        return false;
+    }
+
+    public static int CountAscendingErrors(List<int> list)
+    {
+        int errors = 0;
+        if (list.Count == 0) return 0;
+
+        int previousInt = list[0];
+
+        foreach (int item in list)
+        {
+            if (item < previousInt) errors++;
+            previousInt = item;
+        }
+
+        return errors;
+    }
+
+    public static int CountDescendingErrors(List<int> list)
+    {
+        int errors = 0;
+        if (list.Count == 0) return 0;
+
+        int previousInt = list[0];
+
+        foreach (int item in list)
+        {
+            if (item > previousInt) errors++;
+            previousInt = item;
+        }
+
+        return errors;
+    }
+
+    public static int CountDistanceErrors(List<int> list)
+    {
+        int errors = 0;
+        if (list.Count == 0) return 0;
+
+        int previousInt = list[0];
+
+        for (int i = 1; i < list.Count; i++)
+        {
+            var item = list[i];
+            var distance = Math.Abs(previousInt - item);
+            if (0 == distance || distance > 3) errors++;
+            previousInt = item;
+        }
+
+        return errors;
+    }
+}
+
