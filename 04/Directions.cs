7c38c4ceb05b273c6b855ae3387e7263928d184f
HAEMIC <Michael.Haemmerle@illwerkevkw.at>
2024-12-04 15:54:22 +0100

    added day 4

---
+++ 04/Directions.cs
@@ -0,0 +1,49 @@
+﻿using System.Diagnostics.Tracing;
+
+namespace _04;
+
+public class Directions
+{
+    public static bool CheckForNextLetter(Direction direction, (int, int) currentPosition, char[,] field, string word)
+    {
+        if (string.IsNullOrEmpty(word))
+        {
+            return true;
+        }
+
+        try
+        {
+            int row = currentPosition.Item1;
+            int col = currentPosition.Item2;
+
+            if (field[row, col] != word[0])
+            {
+                return false;
+            }
+
+            var addStep = Vector[direction];
+            var nextPosition = (row + addStep.Item1, col + addStep.Item2);
+
+            return CheckForNextLetter(direction, nextPosition, field, word.Substring(1));
+        }
+        catch (IndexOutOfRangeException)
+        {
+            return false;
+        }
+    }
+
+    public enum Direction { Right, UpRight, Up, UpLeft, Left, DownLeft, Down, DownRight }
+
+    public static Dictionary<Direction, (int, int)> Vector = new()
+    {
+        {Direction.Right, (1, 0) },
+        {Direction.UpRight, (1, -1) },
+        {Direction.Up, (0, -1) },
+        {Direction.UpLeft, (-1, -1) },
+        {Direction.Left, (-1, 0) },
+        {Direction.DownLeft, (-1, 1) },
+        {Direction.Down, (0, 1) },
+        {Direction.DownRight, (1, 1) }
+    };
+}
+
---
+++ 04/Program.cs
@@ -0,0 +1,57 @@
+﻿using _04;
+using static _04.Directions;
+
+var input = string.Empty;
+
+using (StreamReader reader = new StreamReader("../../../input.txt"))
+{
+    input = reader.ReadToEnd();
+}
+
+var lines = input.Split('\n').Select(x => x.Trim()).ToArray();
+
+
+var yLength = lines.Length;
+var xLength = lines[0].Length;
+
+char[,] field = new char[yLength, xLength];
+
+for (int row = 0; row < yLength; row++)
+{
+    for (int col = 0; col < xLength; col++)
+    {
+        field[row, col] = lines[row][col];
+    }
+}
+
+int sum1 = 0;
+int sum2 = 0;
+
+for (int row = 0; row < yLength; row++)
+{
+    for (int col = 0; col < xLength; col++)
+    {
+        foreach (var direction in Directions.Vector.Keys)
+        {
+            if (Directions.CheckForNextLetter(direction, (col, row), field, "XMAS"))
+            {
+                sum1++;
+            }
+        }
+
+        var DownRightMas = Directions.CheckForNextLetter(Direction.DownRight, (col, row), field, "MAS");
+        var DownLeftMas = Directions.CheckForNextLetter(Direction.DownLeft, (col + 2, row), field, "MAS");
+        var DownRightSam = Directions.CheckForNextLetter(Direction.DownRight, (col, row), field, "SAM");
+        var DownLeftSam = Directions.CheckForNextLetter(Direction.DownLeft, (col + 2, row), field, "SAM");
+
+        if ((DownRightMas || DownRightSam) && (DownLeftMas || DownLeftSam))
+        {
+            sum2++;
+        }
+
+}
+}
+
+Console.WriteLine($"solution for 1: {sum1}");
+Console.WriteLine($"solution for 2: {sum2}");
+
