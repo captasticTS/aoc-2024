d35b8bd456e52bf67adcad8edfac50b7a23a9caa
HAEMIC <Michael.Haemmerle@illwerkevkw.at>
2024-12-06 18:17:01 +0100

    started day 06

---
+++ 06/Program.cs
@@ -0,0 +1,30 @@
+﻿var input = string.Empty;
+
+using (StreamReader reader = new StreamReader("../../../input.txt"))
+{
+    input = reader.ReadToEnd();
+}
+
+List<string> lines = input.Split("\n").Select(x => x.Trim()).ToList();
+
+var rows = lines.Count;
+var columns = lines[0].Length;
+
+char[,] field = new char[rows, columns];
+
+for (int row = 0; row < rows; row++)
+{
+    for (int col = 0; col < columns; col++)
+    {
+        field[row, col] = lines[row][col];
+    }
+}
+
+for (int row = 0; row < rows; row++)
+{
+    for (int col = 0; col < columns; col++)
+    {
+        Console.Write(field[row, col]);
+    }
+    Console.WriteLine();
+}
