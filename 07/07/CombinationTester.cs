4bf74cd6c2d13e3495e4e47bcc2163f4f6f2c5a2
captastic <captastic@gmx.at>
2024-12-08 20:54:38 +0100

    added day 07

---
+++ 07/07/CombinationTester.cs
@@ -0,0 +1,44 @@
+﻿namespace _07;
+
+public static class CombinationTester
+{
+    public static (long, List<long>) GetNumbers(string input)
+    {
+        var inputSplit = input.Split(":").Select(x => x.Trim()).ToList();
+        
+        var result = long.Parse(inputSplit[0]);
+        
+        var combinators = inputSplit[1].Split(" ").Select(x => long.Parse(x)).ToList();
+        
+        return (result, combinators);
+    }
+
+    public static bool TestCombination(long result, List<long> combinators, bool allowConcatination)
+    {
+        if (combinators.Count == 2)
+        {
+            return combinators[0] + combinators[1] == result || 
+                   combinators[0] * combinators[1] == result ||
+                   allowConcatination && long.Parse(combinators[0].ToString() + combinators[1].ToString()) == result;
+        }
+        
+        var worksWithPlus = TestCombination(result, new List<long> {combinators[0] + combinators[1]}
+            .Concat(combinators.Skip(2)).ToList(), allowConcatination);
+        
+        var worksWithTimes = TestCombination(result, new List<long> {combinators[0] * combinators[1]}
+            .Concat(combinators.Skip(2)).ToList(), allowConcatination);
+        
+        var worksWithConcatination = allowConcatination && TestCombination(result, new List<long> 
+                {long.Parse(combinators[0].ToString() + combinators[1].ToString())}
+                .Concat(combinators.Skip(2)).ToList(), allowConcatination);
+        
+        return worksWithPlus || worksWithTimes || worksWithConcatination;
+    }
+
+    public static bool TestCombination(string input, bool allowConcatination)
+    {
+        var inputSplit = GetNumbers(input);
+        
+        return TestCombination(inputSplit.Item1, inputSplit.Item2, allowConcatination);
+    }
+}
---
+++ 07/07/Program.cs
@@ -0,0 +1,28 @@
+﻿using System.Threading.Channels;
+using _07;
+
+var input = string.Empty;
+
+using (StreamReader reader = new StreamReader("../../../../input.txt"))
+{
+    input = reader.ReadToEnd();
+}
+
+List<string> equationsInput = input.Split("\n").Select(x => x.Trim()).ToList();
+
+long sum1 = 0;
+long sum2 = 0;
+
+foreach (var equation in equationsInput)
+{
+    var inputSplit = CombinationTester.GetNumbers(equation);
+    
+    sum1 += CombinationTester.TestCombination(inputSplit.Item1, inputSplit.Item2, false) ? 
+        inputSplit.Item1 : 0;
+    
+    sum2 += CombinationTester.TestCombination(inputSplit.Item1, inputSplit.Item2, true) ? 
+        inputSplit.Item1 : 0;
+}
+
+Console.WriteLine($"result of first exercise: {sum1}");
+Console.WriteLine($"result of second exercise: {sum2}");
